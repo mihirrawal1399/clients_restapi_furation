d5c65c33e4c971d2d3602ebc43d6dc3ea7f3c821
MihirHefty <54273763+mihirrawal1399@users.noreply.github.com>
2023-02-18 14:18:41 +0530

    resuable api call

--- lib/src/clients/client_api.dart
+++ lib/src/clients/client_api.dart
@@ -5,7 +5,34 @@ import 'package:http/http.dart' as http;
 
 class ApiClient {
   static const baseUrl = 'https://example.com/api';
-  // ApiClient({required this.baseUrl})
+
+  static Future<List<Client>> getClientsReusable({int limit = 5, int? lastId, String? colm}) async {
+
+    String type ='';
+    if (lastId!=null && colm!=null) {
+      type = 'sorted_paginated';
+    } else if (colm!=null) {
+      type =  'sorted';
+    } else if (lastId!=null) {
+      type = 'paginated';
+    }
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/clients/get/$type'),
+      body: jsonEncode({
+        'limit': limit,
+        'last_id' : lastId,
+        'sort_by_col' : colm,
+      })
+    );
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      return List<Client>.from(jsonData.map((json) => Client.fromJson(json)));
+    } else {
+      throw Exception('Failed to get clients');
+    }
+  }
 
   static Future<List<Client>> getClients({int limit = 5,}) async {
     final response = await http.post(
